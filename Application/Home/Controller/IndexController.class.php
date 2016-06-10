44feec282cead4bcaab59d7a31adc457ca37cc70
songbaby <472711629@qq.com>
2016-06-10 11:52:58 +0800

    完善map

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,10 +4,52 @@ use Think\Controller;
 class IndexController extends Controller {
 
     public function index(){
-        $info_cooler = M('info_cooler')->order('id DESC')->select();
+      //  $info_cooler = M('info_cooler')->order('id DESC')->select();
        // dump($info_cooler);die;
 
-        $this->info_cooler = $info_cooler;
+       // $this->info_cooler = $info_cooler;
+        $this->display('index');
+
+
+        //$loc_county = M('loc_county')->order('id DESC')->select();
+        //dump($loc_county);
+
+
+       // $loc_town = M('loc_town')->order('id DESC')->select($loc_county);
+       // dump($loc_town);
+
+       // $arr = array();
+       // $arr['ddd'] = $loc_town;
+       // dump($arr);
+
+       // foreach($loc_county as $item) {
+            //dump($item['pid']);
+       // }
+    }
+
+
+    function select(){
+
+        if( !empty($_GET['town']) and !empty($_GET['county']) ){//显示冷库信息
+            $cooler = M('info_cooler')->where("town ='%s' and county = '%s'",$_GET['town'] ,$_GET['county'])->order('id DESC')->select();
+            $this->cooler = json_encode($cooler);
+
+            $loc_town = M('loc_town')->where("pid =%d",$_GET['county'] )->order('id DESC')->select();
+            //dump($loc_town);
+            $this->loc_town = $loc_town;
+
+        }
+        else  if(empty($_GET['town']) and !empty($_GET['county'])){//显示town标签
+
+            $cooler = M('info_cooler')->where("town ='%s' and county = '%s'",$_GET['town'] ,$_GET['county'])->order('id DESC')->select();
+            $this->cooler = json_encode($cooler);
+
+            $loc_town = M('loc_town')->where("pid =%d",$_GET['county'] )->order('id DESC')->select();
+            //dump($loc_town);
+            $this->loc_town = $loc_town;
+        }
+
+
         $this->display('index');
     }
 
@@ -15,6 +57,4 @@ class IndexController extends Controller {
     public  function map(){
         $this->display();
     }
-
-
 }
--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -9,52 +9,42 @@
 
 
 <body>
-<?php dump($info_cooler) ?>;
 
 
 <div id="top">
 
-    <form id="form1" name="form1" method="post" action="">
 
       <div align="center">
-        <p>
-          <input type="text" name="search" id="search" />
-          <input type="submit" name="search2" id="search2" value="search" />
-            <div>
-          <ul>
-              <li style="float:right;">
-                   列表
-              </li>
-              <li style="float:right;">
-                  -
-              </li>
-              <li style="float:right;">
-                  地图
-              </li>
-          </ul>
 
-            </div>
-        </p>
-      </div>
-    </form>
+          <div style="text-align: right">
+              登录 注册
+              </div>
+          <div style="text-align: left">
+
+          <a href="/cooler/index.php/Home/Index/select/county/1" >金乡县</a>
+              <a href="/cooler/index.php/Home/Index/select/county/2" >成武县</a>
+              <a href="/cooler/index.php/Home/Index/select/county/3" >寿光市</a>
+          </div>
+
+          <div style="text-align: left">
+              <?php if(is_array($loc_town)): foreach($loc_town as $k=>$vo): ?><a href="/cooler/index.php/Home/Index/select/county/1/town/<?php echo ($vo[id]); ?>"> <?php echo ($vo[name]); ?>,</a><?php endforeach; endif; ?>
+        </div>
+        
+        </div>
+
+
+
+
 
 </div>
 
 <div id="content">
-    <div id="left">
-    <ul>
-        <li><a onclick="addp()">金乡镇</a></li>
-        <li><a href="#">马庙</a></li>
-        <li><a href="#">鱼山</a></li>
-    </ul>
-</div>
-    <div id="right_map"></div>
-    <div id="right_list"></div>
+
 </div>
 
 
 <div id="bottum">
-    网络文化经营许可证：浙网文[2013]0268-027号|增值电信业务经营许可证：浙B2-20080224|信息网络传播视听节目许可证：1109364号|互联网违法和不良信息举报电话:0571-81683755 blxx@list.alibaba-inc.com
+    网络文化经营许可证：沪[2013]0268-027号|增值电信业务经营许可证：沪A2-20080224|信息网络传播视听节目许可证：1109364号|互联网违法和不良信息举报电话:021-81683755 blxx@list.alibaba-inc.com
 </div>
 
 </body>
@@ -62,29 +52,65 @@
 
 
 
-
 <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=3nbIhiC4xvRt2ofWcRPAo4uj"></script>
+
+
 <script type="text/javascript">
     // 百度地图API功能
 
-    // 百度地图API功能
-    var map = new BMap.Map("right_map");
-    var point = new BMap.Point(116.404, 39.915);
-    map.centerAndZoom(point, 15);
-    // 编写自定义函数,创建标注
     function addMarker(point){
         var marker = new BMap.Marker(point);
         map.addOverlay(marker);
     }
-    // 随机向地图添加25个标注
-    var bounds = map.getBounds();
-    var sw = bounds.getSouthWest();
-    var ne = bounds.getNorthEast();
-    var lngSpan = Math.abs(sw.lng - ne.lng);
-    var latSpan = Math.abs(ne.lat - sw.lat);
-    for (var i = 0; i < 25; i ++) {
-        var point = new BMap.Point(sw.lng + lngSpan * (Math.random() * 0.7), ne.lat - latSpan * (Math.random() * 0.7));
-        addMarker(point);
+
+    // 百度地图API功能
+    var map = new BMap.Map("content");
+    var point = new BMap.Point(116.235237, 35.079983);
+    map.enableScrollWheelZoom();
+    //map.centerAndZoom(point, 12);
+   // addMarker(point);
+
+
+    function getBoundary(){
+        var bdary = new BMap.Boundary();
+        bdary.get("金乡县", function(rs){       //获取行政区域
+           // map.clearOverlays();        //清除地图覆盖物
+            var count = rs.boundaries.length; //行政区域的点有多少个
+            if (count === 0) {
+                alert('未能获取当前输入行政区域');
+                return ;
+            }
+            var pointArray = [];
+            for (var i = 0; i < count; i++) {
+                var ply = new BMap.Polygon(rs.boundaries[i], {strokeWeight: 2, strokeColor: "blue"}); //建立多边形覆盖物
+                ply.setStrokeStyle("dashed");
+                ply.setFillColor("none");
+                map.addOverlay(ply);  //添加覆盖物
+                pointArray = pointArray.concat(ply.getPath());
+            }
+            map.setViewport(pointArray);    //调整视野
+        });
     }
+
+    function AddCooler()
+    {
+
+
+            // var   user ={"name":"Eric","age":23};
+            var   coolerArray = <?php echo $cooler ?>;
+
+            console.log(coolerArray);
+            for(var i = 0; i<coolerArray.length;i++)
+            {
+                console.log(coolerArray[i].name,coolerArray[i].lat,coolerArray[i].lng);
+                var point = new BMap.Point(coolerArray[i].lng, coolerArray[i].lat);
+                addMarker(point);
+
+            }
+
+    }
+
+    getBoundary();
+    AddCooler();
 
 </script>
