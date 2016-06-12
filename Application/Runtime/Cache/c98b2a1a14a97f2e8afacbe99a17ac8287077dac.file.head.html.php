e32a309d17222b9d7c5b2d8eb25791f3e882b253
songbaby <472711629@qq.com>
2016-06-12 23:05:07 +0800

    add admin module

---
+++ Application/Runtime/Cache/c98b2a1a14a97f2e8afacbe99a17ac8287077dac.file.head.html.php
@@ -0,0 +1,55 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2016-06-12 22:44:35
+         compiled from "./Application/Admin/View\Index\head.html" */ ?>
+<?php /*%%SmartyHeaderCode:23549575d7553363f89-13974799%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'c98b2a1a14a97f2e8afacbe99a17ac8287077dac' => 
+    array (
+      0 => './Application/Admin/View\\Index\\head.html',
+      1 => 1390287770,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '23549575d7553363f89-13974799',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_575d75533a278',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_575d75533a278')) {function content_575d75533a278($_smarty_tpl) {?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <meta http-equiv=content-type content="text/html; charset=utf-8" />
+        <link href="<?php echo @ADMIN_CSS_URL;?>
+admin.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <table cellspacing=0 cellpadding=0 width="100%" 
+               background="<?php echo @ADMIN_IMG_URL;?>
+header_bg.jpg" border=0>
+            <tr height=56>
+                <td width=260><img height=56 src="<?php echo @ADMIN_IMG_URL;?>
+header_left.jpg" 
+                                   width=260></td>
+                <td style="font-weight: bold; color: #fff; padding-top: 20px" 
+                    align=middle>当前用户：<?php echo $_SESSION['mg_username'];?>
+ &nbsp;&nbsp; <a style="color: #fff" 
+                                                        href="" 
+                                                        target=main>修改口令</a> &nbsp;&nbsp; <a style="color: #fff" 
+                                                        onclick="if (confirm('确定要退出吗？')) return true; else return false;" 
+                                                        href="<?php echo @__MODULE__;?>
+/Manager/logout" target=_top>退出系统</a> 
+                </td>
+                <td align=right width=268><img height=56 
+                                               src="<?php echo @ADMIN_IMG_URL;?>
+header_right.jpg" width=268></td></tr></table>
+        <table cellspacing=0 cellpadding=0 width="100%" border=0>
+            <tr bgcolor=#1c5db6 height=4>
+                <td></td>
+            </tr>
+        </table>
+    </body>
+</html><?php }} ?>
---
+++ Application/Runtime/Cache/fcd08bbd10c5b207505995d7de2e17b120fb0362.file.index.html.php
@@ -0,0 +1,44 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2016-06-12 22:44:35
+         compiled from "./Application/Admin/View\Index\index.html" */ ?>
+<?php /*%%SmartyHeaderCode:12372575d755322b785-73466063%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'fcd08bbd10c5b207505995d7de2e17b120fb0362' => 
+    array (
+      0 => './Application/Admin/View\\Index\\index.html',
+      1 => 1390206964,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '12372575d755322b785-73466063',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_575d7553271c8',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_575d7553271c8')) {function content_575d7553271c8($_smarty_tpl) {?><!doctype html public "-//w3c//dtd xhtml 1.0 frameset//en" "http://www.w3.org/tr/xhtml1/dtd/xhtml1-frameset.dtd">
+<html>
+    <head>
+        <meta http-equiv=content-type content="text/html; charset=utf-8" />
+        <meta http-equiv=pragma content=no-cache />
+        <meta http-equiv=cache-control content=no-cache />
+        <meta http-equiv=expires content=-1000 />
+        
+        <title>管理中心 v1.0</title>
+    </head>
+    <frameset border=0 framespacing=0 rows="60, *" frameborder=0>
+        <frame name=head src="<?php echo @__CONTROLLER__;?>
+/head" frameborder=0 noresize scrolling=no>
+            <frameset cols="170, *">
+                <frame name=left src="<?php echo @__CONTROLLER__;?>
+/left" frameborder=0 noresize />
+                <frame name=right src="<?php echo @__CONTROLLER__;?>
+/right" frameborder=0 noresize scrolling=yes />
+            </frameset>
+    </frameset>
+    <noframes>
+    </noframes>
+</html><?php }} ?>
